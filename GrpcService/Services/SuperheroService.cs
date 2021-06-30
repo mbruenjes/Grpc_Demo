80147d0b5257994553ae686e419a42cb9695f3b4
Michael Brünjes <mbruenjes@arcor.de>
2021-06-30 12:01:52 +0200

    Added services and functionality to gRPC and REST service

--- GrpcService/Services/SuperheroService.cs
+++ GrpcService/Services/SuperheroService.cs
@@ -6,20 +6,24 @@ namespace GrpcService.Services
 {
     public class SuperheroService : Superhero.SuperheroBase
     {
-        private SuperheroRepository _repository;
+        private readonly SuperheroRepository _repository;
         
         public SuperheroService()
         {
             _repository = new SuperheroRepository();
         }
-        public override Task<SuperheroResponse> GetSuperhero(SuperheroRequest request, ServerCallContext context)
+        public override async Task<SuperheroResponse> GetSuperhero(SuperheroRequest request, ServerCallContext context)
         {
             var superhero = _repository.GetSuperhero();
-            
-            return new SuperheroResponse()
+
+            return await Task.Run(() => new SuperheroResponse()
             {
-                
-            }
+                Alias = superhero.Alias,
+                Name = superhero.Name,
+                Height = superhero.Height,
+                Weight = superhero.Weight,
+                Universe = superhero.Universe.ToString("G")
+            });
         }
     }
 }
---
+++ RestService/Controllers/SuperheroController.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestService.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class SuperheroController : Controller
+    {
+        private SuperheroRepository _repository;
+        
+        public SuperheroController()
+        {
+            _repository = new SuperheroRepository();
+        }
+        
+        // GET
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_repository.GetSuperhero());
+        }
+    }
+}
